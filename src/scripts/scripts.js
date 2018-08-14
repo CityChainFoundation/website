982df0fbb9c92c1aa7d1c1c9bba122137a7898f4
SondreB <pixelworld@hotmail.com>
2018-08-14 19:06:49 +0200

    Add information regarding donations (#1)
    - Add Libertania logo.
    - Add City Chain Foundation vertical logo.
    - Add chart for donation distribution.
    - Logo for incubator is missing.
    - Updates details on staking.

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -30,3 +30,39 @@ if (videoLink) {
         frame.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
     });
 }
+
+var donationsChartElement = document.getElementById("donationsChart").getContext('2d');
+
+var donationsChart = new Chart(donationsChartElement, {
+    type: 'doughnut',
+    data: {
+        labels: ["Libertania: Startup Fund", "Incubator Program", "City Chain Foundation"],
+        datasets: [{
+            label: "Donations Allocation",
+            backgroundColor: ['#E0E01D', '#252422', '#DFAB3C'],
+            borderColor: '#ffffff',
+            data: [55, 20, 25],
+        }]
+    },
+    options: {
+        legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+                padding: 20,
+                fontSize: 14,
+                fontStyle: 'normal',
+                fontFamily: "'Source Sans Pro', sans-serif"
+            }
+        },
+        tooltips: {
+            enabled: true,
+            mode: 'single',
+            callbacks: {
+                label: function (tooltipItems, data) {
+                    return data.labels[tooltipItems.index] + ': ' + data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index] + '%';
+                }
+            }
+        },
+    }
+});
