a7462f29dc541faf3b57ea6d63b5799207d020f9
Sondre <sondre@outlook.com>
2021-05-24 20:40:48 +0200

    Start to migrate the team info from foundation website to main website

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -66,3 +66,44 @@ var donationsChart = new Chart(donationsChartElement, {
         },
     }
 });
+
+
+
+var donationsChartElement2 = document.getElementById("donationsChart2").getContext('2d');
+
+var donationsChart2 = new Chart(donationsChartElement2, {
+    type: 'doughnut',
+    data: {
+        labels: ["Software Development", "Operations", "Marketing", "Funding (projects)", "Advisors", "Legal", "Community"],
+        datasets: [{
+            label: "Donations Allocation",
+            backgroundColor: ['#FFAA00', '#D0D0A2', '#808000', '#403318', '#201A0C', '#856B27', '#BD9635'],
+            borderColor: '#ffffff',
+            data: [40, 8, 14, 10, 2, 6, 20],
+        }]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            display: true,
+            position: 'right',
+            labels: {
+                padding: 8,
+                fontSize: 14,
+                boxWidth: 24,
+                fontColor: 'white',
+                fontStyle: 'normal',
+                fontFamily: "'Source Sans Pro', sans-serif"
+            }
+        },
+        tooltips: {
+            enabled: true,
+            mode: 'single',
+            callbacks: {
+                label: function (tooltipItems, data) {
+                    return data.labels[tooltipItems.index] + ': ' + data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index] + '%';
+                }
+            }
+        },
+    }
+});
